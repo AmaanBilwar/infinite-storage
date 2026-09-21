ab3cf3f2342debdf2edd9178f55932be69da9a9d
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-09-21 22:04:11 +0000

    create and delte bucket works

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ struct Cli {
 enum Commands {
     ListBuckets,
     CreateBucket { bucket_name: String },
+    DeleteBucket { bucket_name: String },
 }
 
 #[tokio::main]
@@ -21,10 +22,13 @@ async fn main() -> Result<(), s3::Error> {
     let cli = Cli::parse();
     let config = aws_config::load_from_env().await;
     let client = aws_sdk_s3::Client::new(&config);
-    // this needs to be abstracted away eventually
+    // clap needs to be abstracted away eventually
     match cli.command {
         Commands::ListBuckets => {
-            let output = client.list_buckets().send().await?;
+            let output = client
+                .list_buckets()
+                .send()
+                .await?;
             for bucket in output.buckets() {
                 if let Some(name) = bucket.name() {
                     println!("{name}")
@@ -33,9 +37,25 @@ async fn main() -> Result<(), s3::Error> {
         }
         // create bucket
         Commands::CreateBucket { bucket_name } => {
-            let response = client.create_bucket().send().await?;
-            println!("{bucket_name}")
-        }
+            client
+                .create_bucket()
+                .bucket(&bucket_name)
+                .send()
+                .await?;
+
+            println!("Created {bucket_name}");
+            }
+
+        // delete a bucket
+        Commands::DeleteBucket{ bucket_name } => {
+            client
+                .delete_bucket()
+                .bucket(&bucket_name)
+                .send()
+                .await?;
+
+            println!("deleted {bucket_name}");
+            }
     }
 
     Ok(())
